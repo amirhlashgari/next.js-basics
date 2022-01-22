5b85be7dcf7540ef3865e70018cb9e23d2bc7d52
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-22 21:11:32 +0330

    feat: use jsx components in auth page, works as before but styling is a bit different

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
     <div>
         <h1>The auth index page</h1>
         <button onClick={() => Router.push('/')}>Main Page</button>
+        <User name="Amirhosein" age="28" />
     </div>
 );
 

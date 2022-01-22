ce2075d374fa769ce8c5a24b54c0ba551fb1dee7
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-22 21:03:37 +0330

    feat: add navigation to pages, using Link and Router

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Router from 'next/router';
 
 const authIndexPage = () => (
     <div>
         <h1>The auth index page</h1>
+        <button onClick={() => Router.push('/')}>Main Page</button>
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 const indexPage = () => (
     <div>
         <h1>The main page</h1>
+        <p>Go To <Link href='/auth'>Auth</Link></p>
     </div>
 );
 
